b9a1e22a390f3597394d3d821ec0627c256f0131
3SBrandon <bjs9107007@gmail.com>
2025-04-30 05:35:39 +0000

    uhhhhhhhhhhhhhhhhhhhhh

--- APcode.py
+++ APcode.py
@@ -20,7 +20,7 @@ lyrics = [
     "God what have you done You're a pink pony girl And you dance at the club Oh mama I'm just having fun",
     "On the edge of your knife stayin' drunk on your vine. The angels up in the clouds are jealous knowin' we found",
     "Someone pour me up a double shot of whiskey They know me and Jack Daniel's got a history There's a party downtown near 5th Street",
-    "I lose control When you're not next to me I’m fallin’ apart right in front of you can't you see",
+    "I lose control When you're not next to me I'm fallin' apart right in front of you can't you see",
     "Don't ask for all your things back cussin' out my name yeah Just to go and take back what you say Burn all the bridges, don't ask forgiveness",
     "I want you I need you oh God Don't take These beautiful things that I've got Please stay",
     "You say I'll never change I'm just a go around town with some gasoline Just tryin' to bum a flame Gonna burn the whole place down",
@@ -41,15 +41,21 @@ artists = [
 
 def UI():
     opt = input('What game would you like to play. 1)Finish the lyric 2)Guess the song or 3)Guess the artist: ')
-    rounds = input('How many rounds would you like to play: ')
+    try:
+        opt = int(opt)
+    except ValueError:
+        print('INPUT ERROR')
+        UI()
 
+    rounds = input('How many rounds would you like to play: ')
     try:
         rounds = int(rounds)
     except ValueError:
         print('INPUT ERROR')
         UI()
+
     print('\n')
-    if opt == '1':
+    if opt == 1:
         score = 0
         for i in range(int(rounds)):
             score = score + finishLyric()
@@ -57,7 +63,7 @@ def UI():
             print('\n')
         print(f'Your final score is: {score}')
         again()
-    elif opt == '2':
+    elif opt == 2:
         score = 0
         for i in range(int(rounds)):
             score = score + guessSong()
@@ -65,7 +71,7 @@ def UI():
             print('\n')
         print(f'Your final score is: {score}')
         again()
-    elif opt == '3':
+    elif opt == 3:
         score = 0
         for i in range(int(rounds)):
             score = score + guessArtist()
@@ -74,7 +80,7 @@ def UI():
         print(f'Your final score is: {score}')
         again()
     else: 
-        print('INPUT ERROR')
+        print('ERROR')
         UI()
 
 def again():
@@ -84,6 +90,9 @@ def again():
     elif quben == 'NO':
         print('Thanks for playing!!!')
         exit
+    else:
+        print('INPUT ERROR')
+        again()
 
 def guessSong():
     r = random.randint(0,len(songs)-1)
@@ -126,7 +135,7 @@ def guessArtist():
     print(songs[r])
     question = input(f'Who is this song by (Type "HINT" to get one): ')
     if question == 'HINT':
-        question = input(f'A lyric from the song is "{lyrics[r]}". Who made this song: ')
+        question = input(f'HINT - A lyric from the song is "{lyrics[r]}". Who made this song: ')
     if question.upper() == answer.upper():
         print('Correct')
         point = 1
